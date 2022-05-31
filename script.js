269584304ef60bd59a70b72ddbdf683c1b026ba2
ac3bk <85188769+ac3bk@users.noreply.github.com>
2022-05-31 01:17:23 -0400

    Update script.js

--- script.js
+++ script.js
@@ -209,7 +209,7 @@ const option2 = {
 const reveal = function(entries){
   console.log(entries[0])
   if(entries[0].isIntersecting){
-    entries[0].target.classList.remove('section--hidden');
+    //entries[0].target.classList.remove('section--hidden');
     sectIntrObs.unobserve(entries[0].target)
   }
 }
